6dac9ce9a856ddaf757052ab21237138b88c4faf
isaach0011 <ihil17523@740s-pl-177.local>
2016-10-03 12:01:49 -0600

    Linked UpdatedViewController.swift file to the associated ViewController in the Storyboard.

---
+++ TransitionsProject/Controller/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UpdatedViewController.swift
+//  TransitionsProject
+//
+//  Created by Hill, Isaac on 10/3/16.
+//  Copyright © 2016 Hill, Isaac. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
